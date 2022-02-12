6545d33c4d14eac541eb2f8471d1ddb3babd5f27
CECTPAYTKYMHE <91718173+CECTPAYTKYMHE@users.noreply.github.com>
2022-02-13 00:24:09 +0300

    yes

--- settings.py
+++
@@ -1,7 +0,0 @@
-import datetime
-time = round(datetime.datetime.timestamp(datetime.datetime.now()))
-credentialname = 'a_ivanov'
-credentialpass = 'bU=Ub53f$4'
-rabbitmqhost = '10.200.64.192'
-queue = 'sys.queues.ad_debug'
-domain = "@test.local"
